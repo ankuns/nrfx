7ef0a3d5251da6b7b3e332f856a3cb319e982abd
Magdalena Pastula <magdalena.pastula@nordicsemi.no>
2025-04-16 12:25:04 +0200

    NRFX-7275: add BSP dispatcher
    Signed-off-by: Magdalena Pastula <magdalena.pastula@nordicsemi.no>

---
+++ bsp/nrfx_bsp.h
@@ -0,0 +1,17 @@
+/*$$$LICENCE_NORDIC_STANDARD<2025>$$$*/
+
+#ifndef NRFX_BSP_H__
+#define NRFX_BSP_H__
+
+#ifndef NRFX_BSP_NRF_PATH
+#define NRFX_BSP_NRF_PATH "stable/mdk/nrf.h"
+#endif
+
+#ifndef NRFX_BSP_ERRATAS_PATH
+#define NRFX_BSP_ERRATAS_PATH "stable/mdk/nrf_erratas.h"
+#endif
+
+#include NRFX_BSP_NRF_PATH
+#include NRFX_BSP_ERRATAS_PATH
+
+#endif
--- drivers/nrfx_common.h
+++ drivers/nrfx_common.h
@@ -9,7 +9,7 @@
 #include <string.h>
 #include <limits.h>
 
-#include <nrf.h>
+#include <nrfx_bsp.h>
 #include "nrfx_utils.h"
 #include <nrf_peripherals.h>
 #include <nrf_mem.h>
