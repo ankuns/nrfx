0d82464f5ddadf4c7a84dc36505f82955a72241a
Krzysztof Chruściński <krzysztof.chruscinski@nordicsemi.no>
2025-06-09 11:38:44 +0200

    NRFX-NONE: nrfx_spim: Fix MLTPAN-55 condition
    Apply MLTPAN-55 workaround for every configuration.

--- drivers/src/nrfx_spim.c
+++ drivers/src/nrfx_spim.c
@@ -457,23 +457,7 @@ static void spim_configure(nrfx_spim_t const *        p_instance,
 #if NRFX_CHECK(NRF54L_ERRATA_55_ENABLE_WORKAROUND)
     if (nrf54l_errata_55())
     {
-        uint32_t p = prescaler *
-                     ((NRF_SPIM_BASE_FREQUENCY_GET(p_instance->p_reg) ==
-                       NRF_SPIM_BASE_FREQUENCY_16MHZ) ? 2 : 1);
-
-        // First condition (CPHA=0) applies to prescaler 4 (slow instances) and 8.
-        // Second condition (CPHA=1) applies to prescaler 8 (slow instances) and 16.
-        if (((p == 8) &&
-             ((p_config->mode == NRF_SPIM_MODE_0) || (p_config->mode == NRF_SPIM_MODE_2))) ||
-            ((p == 16) &&
-             ((p_config->mode == NRF_SPIM_MODE_1) || (p_config->mode == NRF_SPIM_MODE_3))))
-        {
-            p_cb->apply_errata_nrf54l_55 = 1;
-        }
-        else
-        {
-            p_cb->apply_errata_nrf54l_55 = 0;
-        }
+        p_cb->apply_errata_nrf54l_55 = 1;
     }
 #endif
 
@@ -847,7 +831,7 @@ static nrfx_err_t spim_xfer(NRF_SPIM_Type               * p_spim,
     nrfy_spim_enable(p_spim);
 
 #if NRFX_CHECK(NRF54L_ERRATA_55_ENABLE_WORKAROUND)
-    if (nrf54l_errata_55() && p_cb->apply_errata_nrf54l_55)
+    if (p_cb->apply_errata_nrf54l_55)
     {
         *(volatile uint32_t *)((uintptr_t)p_spim + 0xc80) = 0x82;
     }
@@ -873,7 +857,7 @@ static nrfx_err_t spim_xfer(NRF_SPIM_Type               * p_spim,
     if (!p_cb->handler)
     {
 #if NRFX_CHECK(NRF54L_ERRATA_55_ENABLE_WORKAROUND)
-        if (nrf54l_errata_55() && p_cb->apply_errata_nrf54l_55)
+        if (p_cb->apply_errata_nrf54l_55)
         {
             *(volatile uint32_t *)((uintptr_t)p_spim + 0xc80) = 0;
         }
@@ -978,8 +962,7 @@ void nrfx_spim_abort(nrfx_spim_t const * p_instance)
 static void irq_handler(NRF_SPIM_Type * p_spim, spim_control_block_t * p_cb)
 {
 #if NRFX_CHECK(NRF54L_ERRATA_55_ENABLE_WORKAROUND)
-    if (nrf54l_errata_55() && p_cb->apply_errata_nrf54l_55 &&
-        nrfy_spim_event_check(p_spim, NRF_SPIM_EVENT_END))
+    if (p_cb->apply_errata_nrf54l_55 && nrfy_spim_event_check(p_spim, NRF_SPIM_EVENT_END))
     {
         *(volatile uint32_t *)((uintptr_t)p_spim + 0xc80) = 0;
     }
