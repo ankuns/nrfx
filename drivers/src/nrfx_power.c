dbd00a379f841de68632271a54529e5be3f0f1c8
Norbert Morawski <norbert.morawski@nordicsemi.no>
2025-07-03 14:19:30 +0200

    NRFX-3941: nrf_power / nrfx_power: errata new system
    Signed-off-by: Norbert Morawski <norbert.morawski@nordicsemi.no>

--- drivers/src/nrfx_power.c
+++ drivers/src/nrfx_power.c
@@ -100,7 +100,7 @@ nrfx_err_t nrfx_power_init(nrfx_power_config_t const * p_config)
 #elif defined(REGULATORS_PRESENT)
     nrf_regulators_vreg_enable_set(NRF_REGULATORS, NRF_REGULATORS_VREG_MAIN, p_config->dcdcen);
 #if !defined(NRF_TRUSTZONE_NONSECURE)
-    if (p_config->dcdcen && nrf53_errata_53())
+    if (p_config->dcdcen && NRF_ERRATA_DYNAMIC_CHECK(53, 53))
     {
         *((volatile uint32_t *)0x50004728ul) = 0x1;
     }
--- hal/nrf_power.h
+++ hal/nrf_power.h
@@ -1620,9 +1620,8 @@ NRF_STATIC_INLINE uint32_t nrf_power_rampower_mask_get(NRF_POWER_Type const * p_
 #if NRF_POWER_HAS_DCDCEN_VDDH
 NRF_STATIC_INLINE void nrf_power_dcdcen_vddh_set(NRF_POWER_Type * p_reg, bool enable)
 {
-    if (enable && nrf52_errata_197())
+    if (enable && NRF_ERRATA_DYNAMIC_CHECK(52, 197))
     {
-        // Workaround for anomaly 197 "POWER: DCDC of REG0 not functional".
         *(volatile uint32_t *)0x40000638ul = 1ul;
     }
     p_reg->DCDCEN0 = (enable ? POWER_DCDCEN0_DCDCEN_Enabled : POWER_DCDCEN0_DCDCEN_Disabled) <<
