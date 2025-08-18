37118f973e6d5ee66e9b6883b2308209a139fe25
Magdalena Pastula <magdalena.pastula@nordicsemi.no>
2025-08-18 13:58:56 +0200

    NRFX-7275: add checking if errata symbols exist
    Add checking if given errata symbol is defined before its value
    is checked.
    
    Signed-off-by: Magdalena Pastula <magdalena.pastula@nordicsemi.no>

--- drivers/include/nrfx_nvmc.h
+++ drivers/include/nrfx_nvmc.h
@@ -296,7 +296,7 @@ NRFX_STATIC_INLINE bool nrfx_nvmc_write_done_check(void)
 
 NRFX_STATIC_INLINE uint32_t nrfx_nvmc_uicr_word_read(uint32_t const volatile *address)
 {
-#if NRF91_ERRATA_7_ENABLE_WORKAROUND
+#if NRFX_CHECK(NRF91_ERRATA_7_ENABLE_WORKAROUND)
     bool irq_disabled = __get_PRIMASK() == 1;
     if (!irq_disabled)
     {
@@ -306,7 +306,7 @@ NRFX_STATIC_INLINE uint32_t nrfx_nvmc_uicr_word_read(uint32_t const volatile *ad
 
     uint32_t value = nrf_nvmc_word_read((uint32_t)address);
 
-#if NRF91_ERRATA_7_ENABLE_WORKAROUND
+#if NRFX_CHECK(NRF91_ERRATA_7_ENABLE_WORKAROUND)
     __DSB();
     if (!irq_disabled)
     {
