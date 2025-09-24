6d01d0f59ecaaabb8093ad941d7a7982e76d7abf
Magdalena Pastula <magdalena.pastula@nordicsemi.no>
2025-09-24 13:32:55 +0200

    NRFX-8369: fix nrfx_config filename for nRF54LS05 in templates
    Signed-off-by: Magdalena Pastula <magdalena.pastula@nordicsemi.no>

--- bsp/stable/templates/nrfx_templates_config.h
+++ bsp/stable/templates/nrfx_templates_config.h
@@ -48,7 +48,7 @@
 #elif defined(NRF54LM20A_ENGA_XXAA) && defined(NRF_FLPR)
     #include <nrfx_config_nrf54lm20a_enga_flpr.h>
 #elif defined(NRF54LS05B_ENGA_XXAA) && defined(NRF_APPLICATION)
-    #include <nrfx_config_nrf54ls05b_application.h>
+    #include <nrfx_config_nrf54ls05b_enga_application.h>
 #elif defined(NRF54LV10A_ENGA_XXAA) && defined(NRF_APPLICATION)
     #include <nrfx_config_nrf54lv10a_enga_application.h>
 #elif defined(NRF54LV10A_ENGA_XXAA) && defined(NRF_FLPR)
