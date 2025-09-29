4557d0ef1c5156f71bbdedede88736d22f1e3491
Michał Stasiak <michal.stasiak@nordicsemi.no>
2025-09-29 13:45:14 +0200

    NFX-8397: Remove nRF9230 EngA from nrfx_config
    Removed headers and mentions in code.
    
    Signed-off-by: Michał Stasiak <michal.stasiak@nordicsemi.no>

--- bsp/stable/templates/nrfx_config_ext.h
+++ bsp/stable/templates/nrfx_config_ext.h
@@ -25,22 +25,6 @@
     #elif defined(NRF_SYSCTRL)
         #include <nrfx_config_nrf9220_sysctrl.h>
     #endif
-#elif defined(NRF9230_ENGA_XXAA)
-    #if defined(NRF_APPLICATION)
-        #include <nrfx_config_nrf9230_enga_application.h>
-    #elif defined(NRF_RADIOCORE)
-        #include <nrfx_config_nrf9230_enga_radiocore.h>
-    #elif defined(NRF_CELLCORE)
-        #include <nrfx_config_nrf9230_enga_cellular.h>
-    #elif defined(NRF_SECURE)
-        #include <nrfx_config_nrf9230_enga_secure.h>
-    #elif defined(NRF_FLPR)
-        #include <nrfx_config_nrf9230_enga_flpr.h>
-    #elif defined(NRF_PPR)
-        #include <nrfx_config_nrf9230_enga_ppr.h>
-    #elif defined(NRF_SYSCTRL)
-        #include <nrfx_config_nrf9230_enga_sysctrl.h>
-    #endif
 #elif defined(NRF9230_XXAA)
     #if defined(NRF_APPLICATION)
         #include <nrfx_config_nrf9230_application.h>
