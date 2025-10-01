9159ea0e1040e595a06f19512216eb2502476e18
Marcin Szymczyk <marcin.szymczyk@nordicsemi.no>
2025-10-01 13:40:42 +0200

    NRFX-8398: move irqs headers to a separate directory
    Signed-off-by: Marcin Szymczyk <marcin.szymczyk@nordicsemi.no>

--- bsp/stable/soc/irqs/nrfx_irqs_ext.h
+++ bsp/stable/soc/irqs/nrfx_irqs_ext.h
@@ -4,9 +4,9 @@
 #define NRFX_IRQS_EXT_H__
 
 #if defined(NRF54H20_XXAA) || defined(NRF7140_XXAA) || defined(NRF9220_XXAA)
-    #include <soc/nrfx_irqs_haltium.h>
+    #include "stable/soc/irqs/nrfx_irqs_haltium.h"
 #elif defined(NRF9230_ENGB_XXAA) || defined(NRF9230_XXAA)
-    #include <soc/nrfx_irqs_nrf9230.h>
+    #include "stable/soc/irqs/nrfx_irqs_nrf9230.h"
 #else
     #error "Unknown device."
 #endif
--- bsp/stable/soc/nrfx_irqs.h
+++ bsp/stable/soc/nrfx_irqs.h
@@ -4,73 +4,73 @@
 #define NRFX_IRQS_H__
 
 #if defined(NRF51)
-    #include <stable/soc/nrfx_irqs_nrf51.h>
+    #include "stable/soc/irqs/nrfx_irqs_nrf51.h"
 #elif defined(NRF52805_XXAA)
-    #include <stable/soc/nrfx_irqs_nrf52805.h>
+    #include "stable/soc/irqs/nrfx_irqs_nrf52805.h"
 #elif defined(NRF52810_XXAA)
-    #include <stable/soc/nrfx_irqs_nrf52810.h>
+    #include "stable/soc/irqs/nrfx_irqs_nrf52810.h"
 #elif defined(NRF52811_XXAA)
-    #include <stable/soc/nrfx_irqs_nrf52811.h>
+    #include "stable/soc/irqs/nrfx_irqs_nrf52811.h"
 #elif defined(NRF52820_XXAA)
-    #include <stable/soc/nrfx_irqs_nrf52820.h>
+    #include "stable/soc/irqs/nrfx_irqs_nrf52820.h"
 #elif defined(NRF52832_XXAA) || defined (NRF52832_XXAB)
-    #include <stable/soc/nrfx_irqs_nrf52832.h>
+    #include "stable/soc/irqs/nrfx_irqs_nrf52832.h"
 #elif defined(NRF52833_XXAA)
-    #include <stable/soc/nrfx_irqs_nrf52833.h>
+    #include "stable/soc/irqs/nrfx_irqs_nrf52833.h"
 #elif defined(NRF52840_XXAA)
-    #include <stable/soc/nrfx_irqs_nrf52840.h>
+    #include "stable/soc/irqs/nrfx_irqs_nrf52840.h"
 #elif defined(NRF5340_XXAA_APPLICATION)
-    #include <stable/soc/nrfx_irqs_nrf5340_application.h>
+    #include "stable/soc/irqs/nrfx_irqs_nrf5340_application.h"
 #elif defined(NRF5340_XXAA_NETWORK)
-    #include <stable/soc/nrfx_irqs_nrf5340_network.h>
+    #include "stable/soc/irqs/nrfx_irqs_nrf5340_network.h"
 #elif defined(NRF54H20_XXAA) && defined(NRF_APPLICATION)
-    #include <stable/soc/nrfx_irqs_nrf54h20_application.h>
+    #include "stable/soc/irqs/nrfx_irqs_nrf54h20_application.h"
 #elif defined(NRF54H20_XXAA) && defined(NRF_RADIOCORE)
-    #include <stable/soc/nrfx_irqs_nrf54h20_radiocore.h>
+    #include "stable/soc/irqs/nrfx_irqs_nrf54h20_radiocore.h"
 #elif defined(NRF54H20_XXAA) && defined(NRF_PPR)
-    #include <stable/soc/nrfx_irqs_nrf54h20_ppr.h>
+    #include "stable/soc/irqs/nrfx_irqs_nrf54h20_ppr.h"
 #elif defined(NRF54H20_XXAA) && defined(NRF_FLPR)
-    #include <stable/soc/nrfx_irqs_nrf54h20_flpr.h>
+    #include "stable/soc/irqs/nrfx_irqs_nrf54h20_flpr.h"
 #elif defined(NRF54L05_XXAA) && defined(NRF_APPLICATION)
-    #include <stable/soc/nrfx_irqs_nrf54l05_application.h>
+    #include "stable/soc/irqs/nrfx_irqs_nrf54l05_application.h"
 #elif defined(NRF54L05_XXAA) && defined(NRF_FLPR)
-    #include <stable/soc/nrfx_irqs_nrf54l05_flpr.h>
+    #include "stable/soc/irqs/nrfx_irqs_nrf54l05_flpr.h"
 #elif defined(NRF54L10_XXAA) && defined(NRF_APPLICATION)
-    #include <stable/soc/nrfx_irqs_nrf54l10_application.h>
+    #include "stable/soc/irqs/nrfx_irqs_nrf54l10_application.h"
 #elif defined(NRF54L10_XXAA) && defined(NRF_FLPR)
-    #include <stable/soc/nrfx_irqs_nrf54l10_flpr.h>
+    #include "stable/soc/irqs/nrfx_irqs_nrf54l10_flpr.h"
 #elif defined(NRF54L15_XXAA) && defined(NRF_APPLICATION)
-    #include <stable/soc/nrfx_irqs_nrf54l15_application.h>
+    #include "stable/soc/irqs/nrfx_irqs_nrf54l15_application.h"
 #elif defined(NRF54L15_XXAA) && defined(NRF_FLPR)
-    #include <stable/soc/nrfx_irqs_nrf54l15_flpr.h>
+    #include "stable/soc/irqs/nrfx_irqs_nrf54l15_flpr.h"
 #elif defined(NRF54LM20A_ENGA_XXAA) && defined(NRF_APPLICATION)
-    #include <stable/soc/nrfx_irqs_nrf54lm20a_enga_application.h>
+    #include "stable/soc/irqs/nrfx_irqs_nrf54lm20a_enga_application.h"
 #elif defined(NRF54LM20A_ENGA_XXAA) && defined(NRF_FLPR)
-    #include <stable/soc/nrfx_irqs_nrf54lm20a_enga_flpr.h>
+    #include "stable/soc/irqs/nrfx_irqs_nrf54lm20a_enga_flpr.h"
 #elif defined(NRF54LS05B_ENGA_XXAA) && defined(NRF_APPLICATION)
-    #include <stable/soc/nrfx_irqs_nrf54ls05b_enga_application.h>
+    #include "stable/soc/irqs/nrfx_irqs_nrf54ls05b_enga_application.h"
 #elif defined(NRF54LV10A_ENGA_XXAA) && defined(NRF_APPLICATION)
-    #include <stable/soc/nrfx_irqs_nrf54lv10a_enga_application.h>
+    #include "stable/soc/irqs/nrfx_irqs_nrf54lv10a_enga_application.h"
 #elif defined(NRF54LV10A_ENGA_XXAA) && defined(NRF_FLPR)
-    #include <stable/soc/nrfx_irqs_nrf54lv10a_enga_flpr.h>
+    #include "stable/soc/irqs/nrfx_irqs_nrf54lv10a_enga_flpr.h"
 #elif defined(NRF54LS05B_ENGA_XXAA) && defined(NRF_APPLICATION)
-    #include <stable/soc/nrfx_irqs_nrf54ls05b_enga_application.h>
+    #include "stable/soc/irqs/nrfx_irqs_nrf54ls05b_enga_application.h"
 #elif defined(NRF7120_ENGA_XXAA) && defined(NRF_APPLICATION)
-    #include <stable/soc/nrfx_irqs_nrf7120_enga_application.h>
+    #include "stable/soc/irqs/nrfx_irqs_nrf7120_enga_application.h"
 #elif defined(NRF7120_ENGA_XXAA) && defined(NRF_FLPR)
-    #include <stable/soc/nrfx_irqs_nrf7120_enga_flpr.h>
+    #include "stable/soc/irqs/nrfx_irqs_nrf7120_enga_flpr.h"
 #elif defined(NRF91_SERIES)
-    #include <stable/soc/nrfx_irqs_nrf91.h>
+    #include "stable/soc/irqs/nrfx_irqs_nrf91.h"
 #elif defined(NRF9230_ENGB_XXAA) && defined(NRF_APPLICATION)
-    #include <stable/soc/nrfx_irqs_nrf9230_engb_application.h>
+    #include "stable/soc/irqs/nrfx_irqs_nrf9230_engb_application.h"
 #elif defined(NRF9230_ENGB_XXAA) && defined(NRF_RADIOCORE)
-    #include <stable/soc/nrfx_irqs_nrf9230_engb_radiocore.h>
+    #include "stable/soc/irqs/nrfx_irqs_nrf9230_engb_radiocore.h"
 #elif defined(NRF9230_ENGB_XXAA) && defined(NRF_PPR)
-    #include <stable/soc/nrfx_irqs_nrf9230_engb_ppr.h>
+    #include "stable/soc/irqs/nrfx_irqs_nrf9230_engb_ppr.h"
 #elif defined(NRF9230_ENGB_XXAA) && defined(NRF_FLPR)
-    #include <stable/soc/nrfx_irqs_nrf9230_engb_flpr.h>
+    #include "stable/soc/irqs/nrfx_irqs_nrf9230_engb_flpr.h"
 #else
-    #include <stable/soc/nrfx_irqs_ext.h>
+    #include "stable/soc/irqs/nrfx_irqs_ext.h"
 #endif
 
 #endif // NRFX_IRQS_H__
